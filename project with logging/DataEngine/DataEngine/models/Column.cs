4bd4f8631e36caeeb08d0a76d28624d10b7e8703
MiriamGarfunkel <Hand1e5@mbjcomp.org.il>
2026-02-23 22:31:05 +0200

    Add files via upload

---
+++ project with logging/DataEngine/DataEngine/models/Column.cs
@@ -0,0 +1,8 @@
+﻿namespace DataEngine.models
+{
+    public class Column
+    {
+        public string Name { get; set; }
+        public DataType DataType { get; set; }
+    }
+}
---
+++ project with logging/DataEngine/DataEngine/models/DataBase.cs
@@ -0,0 +1,39 @@
+﻿using DataEngine.logging;
+
+namespace DataEngine.models
+{
+    public class DataBase
+    {
+        public string Name { get; set; }
+        public Dictionary<string, Table> Tables { get; set; } = new Dictionary<string, Table>();
+
+        private readonly List<IChangeListener> _listeners = new();
+
+        public void Subscribe(IChangeListener listener)
+        {
+            _listeners.Add(listener); 
+        }
+
+        public void Notify(string actionType, string tableName, int affectedRows)
+        {
+            foreach (var listener in _listeners)
+            {
+                listener.OnChange(actionType, tableName, affectedRows);
+            }
+        }
+        public void RegisterTable(Table table)
+        {
+            Tables[table.Name] = table;
+        }
+
+        public void RemoveTable(string tableName)
+        {
+            Tables.Remove(tableName);
+        }
+
+        public Table GetTable(string tableName)
+        {
+            return Tables.TryGetValue(tableName, out var table) ? table : null;
+        }
+    }
+}
