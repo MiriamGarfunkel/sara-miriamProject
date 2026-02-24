66610cd96ed429d9138ca339c3141f7d89868ba8
MiriamGarfunkel <Hand1e5@mbjcomp.org.il>
2026-02-25 00:27:37 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using DataEngine;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.SetIsOriginAllowed(origin => origin.StartsWith("http://localhost"))
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowAngular");
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
